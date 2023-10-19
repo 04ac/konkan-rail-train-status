4c41dedd8aa7e9d406e630a84b77014b5da78b85
04ac <62027600+04ac@users.noreply.github.com>
2023-10-19 15:55:49 +0530

    Update URLs to use v3 of API

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   static const FETCH_TRAINS_API =
-      "https://konkan-railway-api.vercel.app/api/fetchTrains";
+      "https://konkan-railway-api.vercel.app/api/v3/fetchTrains";
 
   static const FETCH_STATIONS_API =
-      'https://konkan-railway-api.vercel.app/api/fetchStations';
+      'https://konkan-railway-api.vercel.app/api/v3/fetchStations';
 }
